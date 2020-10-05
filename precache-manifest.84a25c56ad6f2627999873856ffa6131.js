6709e3795fd71e800bb1b1d1fe072e0b7312a23c
ebinxavier <ebinx7@gmail.com>
2020-10-05 21:03:32 +0400

    Updates

---
+++ precache-manifest.84a25c56ad6f2627999873856ffa6131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d7ed1208f50b0f06b7734220acc82b1",
+    "url": "/youtube/index.html"
+  },
+  {
+    "revision": "335c474c3bfea2a75a40",
+    "url": "/youtube/static/css/main.051fdf6f.chunk.css"
+  },
+  {
+    "revision": "adb44918b636bdbc93fa",
+    "url": "/youtube/static/js/2.5f258d42.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube/static/js/2.5f258d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "335c474c3bfea2a75a40",
+    "url": "/youtube/static/js/main.6797e712.chunk.js"
+  },
+  {
+    "revision": "66d15f02628b16631539",
+    "url": "/youtube/static/js/runtime-main.fd6ec26e.js"
+  }
+]);
