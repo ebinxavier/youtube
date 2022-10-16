9e50f1d81daf243e0f3b033da6d0a819db12dfdd
Manu A T <38802051+ManuAT@users.noreply.github.com>
2022-10-16 22:50:30 +0530

    Update serviceWorker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -20,6 +20,16 @@ const isLocalhost = Boolean(
     )
 );
 
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.0/8 are considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
